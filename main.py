b942cd3fc84bb44998854c8b460c44df13413a9c
KassidyTang <102830229+kassidytang@users.noreply.github.com>
2022-04-01 20:46:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+userAnswer = input("How are you today?")
+print("Same. I'm " + userAnswer + " too.")
+
+print("welp life goes on")
+
+userAnswer = input("What is your favorite color?")
+print("bold opinion")
+
+userAnswer = input("Why did the chicken cross the road?")
+print("no.")
+
+userAnswer = input("Let's get into the deep questions. Are you happy?")
+print("hmmm..." + userAnswer + "...huh...interesting")
+
+userAnswer = input("Why do you feel that way?")
+print(userAnswer + " damn that's deep")
+
+userAnswer = input("Next question. Do you know what you're doing with your life?")
+print("yikes")
+
+userAnswer = input("Is it almost time?")
+print("time to shutdown..")
